605678de205beb58e5b5d7deaa1f19444b050165
igamenovoer <igame@where.is>
2024-07-31 17:36:55 +0000

    done with vision test

--- test/utest-vision.cpp
+++ test/utest-vision.cpp
@@ -1,5 +1,4 @@
 #include <filesystem>
-#include <opencv2/core/hal/interface.h>
 #include <string>
 #include <vector>
 #include <catch2/catch_all.hpp>
@@ -12,9 +11,14 @@
     static_assert(0, "IGCCLIB_DATA_DIR is not defined");
 #endif
 
+// required definitions of output directory in IGCCLIB_OUTPUT_DIR, otherwise raise compile error
+#ifndef IGCCLIB_OUTPUT_DIR
+    static_assert(0, "IGCCLIB_OUTPUT_DIR is not defined");
+#endif
+
 namespace fs = std::filesystem;
 
-const std::string output_dir = "tmp/output/vision";
+const std::string output_dir = fs::path(IGCCLIB_OUTPUT_DIR)/"vision";
 const std::string data_dir = IGCCLIB_DATA_DIR;
 const std::string image_filename = "05673d9a-caba-4f8a-99ef-55763b124d31.png";
 
@@ -22,18 +26,26 @@ const std::string image_filename = "05673d9a-caba-4f8a-99ef-55763b124d31.png";
 const std::string image_path = fs::path(data_dir) / image_filename;
 
 void do_resize(const cv::Mat& img, int width, int height){
-    spdlog::info("do_resize image to {}x{}", width, height);
+    spdlog::info("do_resize image to {}x{}, number of channels {}", width, height, img.channels());
     // resize it
-    igcclib::ImageRGBA_u _resized_img;
-    igcclib::ImageRGBA_u _img;
+    igcclib::ImageRGBA_d _resized_img;
+    igcclib::ImageRGBA_d _img;
     _img.from_linear_buffer(std::vector<uint8_t>(img.data, img.data + img.total() * img.elemSize()), 
-            img.cols, img.rows, img.channels(), 0, 255, 0, 255);
+            img.cols, img.rows, img.channels(), 0, 255, 0, 1);
     igcclib::resize_image(_img, _resized_img, width, height);
+
+    // {
+    //     _resized_img = _img;
+    //     cv::Mat single_channel_img(_resized_img.get_height(), _resized_img.get_width(), CV_8UC1, _resized_img.g.data());
+    //     cv::imwrite(fs::path(output_dir) / "single_channel.png", single_channel_img);
+    //     spdlog::info("number of channels = {}", _resized_img.get_number_of_channels());
+    // }
+
 
     // combine it back to cv::Mat
     std::vector<uint8_t> resized_img_data;
-    _resized_img.to_linear_buffer(resized_img_data, 0, 255, 0, 255);
-    cv::Mat resized_img(_resized_img.get_height(), _resized_img.get_width(), CV_8UC1, resized_img_data.data());
+    _resized_img.to_linear_buffer(resized_img_data, 0, 1, 0, 255);
+    cv::Mat resized_img(_resized_img.get_height(), _resized_img.get_width(), CV_8UC4, resized_img_data.data());
     cv::imwrite(fs::path(output_dir) / "resized.png", resized_img);
     cv::imwrite(fs::path(output_dir) / "original.png", img);
 }
