8988531e2f743a4da0e3d3468c4b6e826c5f7cf1
igamenovoer <igamenover@gmail.com>
2024-08-02 00:17:59 +0800

    removed unused files

--- igcclib-config.cmake
+++
@@ -1,4 +0,0 @@
-foreach(component ${@project_name@_FIND_COMPONENTS})
-#   set(igcclib_FIND_COMPONENTS_STRING "${igcclib_FIND_COMPONENTS_STRING} ${component}")
-    include(${CMAKE_CURRENT_LIST_DIR}/igcclib-config-${component}.cmake)
-endforeach()
